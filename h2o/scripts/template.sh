955bdc75cd1292f05da6b8502adbcfe3219c8ec1
Giovanni Baratta <bgiovanni@vmware.com>
2023-11-09 15:34:11 +0100

    Add template file for scripts

---
+++ h2o/scripts/template.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+function main() {
+}
+
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
+info() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*"
+}
+
+#######################################
+# Entrypoint
+#######################################
+main "$@"
