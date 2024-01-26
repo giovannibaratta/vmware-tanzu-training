c2211c44d632d713f43ee7e237cf45865af77a62
Giovanni Baratta <bgiovanni@vmware.com>
2024-01-26 10:19:11 +0100

    tf(01-tks-nsx): deploy IDP

---
+++ terraform/stages/01-tkgs-nsx/idp.tf
@@ -0,0 +1,14 @@
+module "keycloak" {
+  source = "github.com/giovannibaratta/vmware-tanzu-training//terraform/modules/keycloak?ref=keycloak-v0.0.1&depth=1"
+
+  fqdn = "keycloak.${var.domain}"
+
+  vm_authorized_key = var.vm_authorized_key
+
+  vsphere = {
+    datastore_id     = data.vsphere_datastore.datastore.id
+    resource_pool_id = vsphere_resource_pool.harbor.id
+    network_id       = data.vsphere_network.mgmt_segment.id
+    template_id      = vsphere_content_library_item.ubuntu2204.id
+  }
+}
