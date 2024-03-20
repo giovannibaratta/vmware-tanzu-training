78faed099060881663eb141eda3fa4b6d2617ed8
Giovanni Baratta <bgiovanni@vmware.com>
2024-03-20 18:45:38 +0100

    tf(harbor-standalone): rename harbor_admin_passowrd to harbor_admin_password

--- terraform/modules/harbor-standalone/outputs.tf
+++ terraform/modules/harbor-standalone/outputs.tf
@@ -2,6 +2,6 @@ output "harbor_instance_ip" {
   value = vsphere_virtual_machine.harbor.default_ip_address
 }
 
-output "harbor_admin_passowrd" {
+output "harbor_admin_password" {
   value = random_password.admin_password.result
 }
